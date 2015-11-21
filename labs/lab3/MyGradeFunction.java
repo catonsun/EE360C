0fa93d71a75d680b06bf56026184723b19e5aa55
Anthony Weems <amlweems@gmail.com>
2015-11-21 11:38:10 -0600

    Non-decreasing function

--- labs/lab3/MyGradeFunction.java
+++ labs/lab3/MyGradeFunction.java
@@ -2,18 +2,20 @@ import java.util.Random;
 
 public class MyGradeFunction implements GradeFunction{
 
-    private final int MAX_GRADE = 10;
     private int numClasses;
     private int maxGrade;
-    private Random rng;
 
     public MyGradeFunction(int n, int g) {
-        this.rng = new Random(4);
         this.numClasses = n;
         this.maxGrade = g;
     }
 
     public int grade(int classID, int hours) {
-        return rng.nextInt(MAX_GRADE + 1);
+        Random rng = new Random(classID);
+        int grade = 0;
+        for (int i = 0; i < hours; i++) {
+            grade += nextInt(2);
+        }
+        return grade;
     }
 }
